e69603edf516f7aa1c19e9a3f92eef25bbaca7c3
Jason Acido <jasonacido@gmail.com>
2019-03-07 23:09:27 +0800

    Updated .gitignore and webpack config

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -21,8 +21,55 @@ module.exports = {
                 use: [ 'babel-loader', 'eslint-loader' ]
             },
             {
-                test: /\.less$/,
-                use: [ 'style-loader', 'css-loder', 'less-loader' ]
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loder' ]
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            // Inline files smaller than 10 kB
+                            limit: 10 * 1024,
+                            noquotes: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            // Inline files smaller than 10 kB
+                            limit: 10 * 1024
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                enabled: false
+                                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+                                // Try enabling it in your environment by switching the config to:
+                                // enabled: true,
+                                // progressive: true,
+                            },
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            optipng: {
+                                optimizationLevel: 7
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            }
+                        }
+                    }
+                ]
             }
         ]
     },
